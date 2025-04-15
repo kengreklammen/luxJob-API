3ca6744370a46e1468d616e4181614c0518ea91e
kengreklammen <6339237+kengreklammen@users.noreply.github.com>
2025-04-15 10:20:22 +0200

    Small change.

--- tesssst_with_httr.R
+++ tesssst_with_httr.R
@@ -2,7 +2,7 @@ library(httr2)
 
 req <- request("http://127.0.0.1:8008/log")
 
-req |> req_body_json(list(user_id = 4, query = "engineering"))
+req |> req_body_json(list(user_id = 4, query = "avionics"))
 #> <httr2_request>
 #> POST https://r-project.org
 #> Body: json encoded data
