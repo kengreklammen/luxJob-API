c8b51d6410e76c8e479de3a6300e975e469db1af
kengreklammen <6339237+kengreklammen@users.noreply.github.com>
2025-04-15 09:25:09 +0200

    Finished API endpoints.

--- plumber.R
+++ plumber.R
@@ -2,41 +2,97 @@ library(plumber)
 # plumber.R
 
 
-#* @apiTitle Basic Plumber API
-#* @apiDescription This is a simple API to demonstrate the use of plumber.
+#* @apiTitle LuxJob API
+#* @apiDescription This is an API to retrieve vacancy-related data from the ADEM database.
 #* @apiVersion 1.0.0
-#* @apiContact pierrick.kinif@datagrowth.io
 #* @apiLicense MIT
 
-#* Echo the parameter that was sent in
-#* @param msg:string  The message to echo back.
-#* @get /echo
-function(msg=""){
-	list(msg = paste0("The message is: '", msg, "'"))
+
+#* Get all skills.
+#* @param limit:number The number of max results.
+#* @get /skills
+function(limit = 100) {
+	luxJob::get_skills(as.numeric(limit))
 }
 
-#* Add two numbers
-#* @param a:number The first number to add.
-#* @param b:number The second number to add.
-#* @get /add
+#* Get a skill by ID.
+#* @param skill_id:string The ID of a particular skill.
+#* @get /askill
+function(skill_id) {
+	luxJob::get_skill_by_id(skill_id)
+}
+
+#* Get all companies.
+#* @param limit:number The number of max results.
+#* @get /companies
+function(limit = 100) {
+	luxJob::get_companies(as.numeric(limit))
+}
+
+#* Get the details of a company.
+#* @param company_id:integer The ID of a particular company.
+#* @get /companydetails
+function(company_id) {
+	luxJob::get_company_details(as.integer(company_id))
+}
+
+#* Get a filtered vacancy list.
+#* @param company_id:integer The ID of a particular company.
+#* @param skill:string The skill.
+#* @param canton:string The canton.
+#* @param limit:integer The max number of results.
+#* @get /vacancies
+function(skill=NULL, company_id=NULL, canton=NULL, limit=100) {
+	luxJob::get_vacancies(skill, company_id, canton, as.integer(limit))
+}
+
+#* Get vacancy by ID.
+#* @param vacancy_id:integer The ID of a particular vacancy.
+#* @get /vacancy
+function(vacancy_id) {
+	luxJob::get_vacancy_by_id(as.integer(vacancy_id))
+}
+
+#* Get learning tracks.
+#* @param skill:string The ID of a particular skill.
+#* @get /learningtracks
+function(skill = NULL) {
+	luxJob::get_learning_tracks(skill)
+}
+
+#* Get a particular learning track.
+#* @param track_id:integer The ID of a particular learning track.
+#* @get /learningtrack
+function(track_id = NULL) {
+	luxJob::get_learning_track_by_id(as.integer(track_id))
+}
+
+#* Get a filtered list of books.
+#* @param skill:string The particular skill.
+#* @get /books
+function(skill = NULL) {
+	luxJob::get_books(skill)
+}
+
+#* Get a particular book.
+#* @param book_id:integer The ID of a particular book.
+#* @get /book
+function(book_id = NULL) {
+	luxJob::get_book_by_id(as.integer(book_id))
+}
+
+#* Post a search into the log system.
+#* @param user_id:integer The ID of a particular user.
+#* @param query:string The search string to send to the log.
+#* @post /log
+function(user_id=NULL, query=NULL) {
+	return(luxJob::log_search(as.integer(user_id), query))
+}
+
+#* Add two numbers. # This is just a simple endpoint for testing POST requests.
+#* @param a:integer A number.
+#* @param b:integer Another number.
+#* @post /add
 function(a, b) {
-	as.numeric(a) + as.numeric(b)
-}
-
-#* Plot out data from the iris dataset - without error handling
-#* @param spec:string If provided, filter the data to only this species (e.g. 'setosa')
-#* @get /plot
-#* @serializer png
-function(spec){
-	myData <- iris
-	title <- "All Species"
-	
-	# Filter if the species was specified
-	if (!missing(spec)) {
-		title <- paste0("Only the '", spec, "' Species")
-		myData <- subset(iris, Species == spec)
-	}
-	
-	plot(myData$Sepal.Length, myData$Petal.Length,
-			 main = title, xlab = "Sepal Length", yla = "Petal Length")
+	return(as.integer(a)+as.integer(b))
 }
---
+++ tesssst_with_httr.R
@@ -0,0 +1,11 @@
+library(httr2)
+
+req <- request("http://127.0.0.1:8008/log")
+
+req |> req_body_json(list(user_id = 4, query = "engineering"))
+#> <httr2_request>
+#> POST https://r-project.org
+#> Body: json encoded data
+#> 
+resp <- req_perform(req)
+resp
