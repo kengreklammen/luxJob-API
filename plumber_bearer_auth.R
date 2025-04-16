f303b332164fc9720d90c76bc775b3dcf3cc0b33
kengreklammen <6339237+kengreklammen@users.noreply.github.com>
2025-04-16 10:47:52 +0200

    Added the verify_token function/file.

--- plumber_bearer_auth.R
+++ plumber_bearer_auth.R
@@ -50,22 +50,28 @@ function(res, req, limit = 100) {
 #* Get a skill by ID.
 #* @param skill_id:string The ID of a particular skill.
 #* @get /askill
-function(skill_id) {
-	luxJob::get_skill_by_id(skill_id)
+function(res, req, skill_id) {
+	auth_helper(res, req, function(skill_id) {
+		luxJob::get_skill_by_id(skill_id)
+	}, skill_id = skill_id)
 }
 
 #* Get all companies.
 #* @param limit:number The number of max results.
 #* @get /companies
-function(limit = 100) {
-	luxJob::get_companies(as.numeric(limit))
+function(res, req, limit = 100) {
+	auth_helper(res, req, function(limit) {
+		luxJob::get_companies(as.numeric(limit))
+	}, limit = limit)
 }
 
 #* Get the details of a company.
 #* @param company_id:integer The ID of a particular company.
 #* @get /companydetails
-function(company_id) {
-	luxJob::get_company_details(as.integer(company_id))
+function(res, req, company_id) {
+	auth_helper(res, req, function(company_id) {
+		luxJob::get_company_details(as.integer(company_id))
+	}, company_id = company_id)
 }
 
 #* Get a filtered vacancy list.
@@ -74,57 +80,73 @@ function(company_id) {
 #* @param canton:string The canton.
 #* @param limit:integer The max number of results.
 #* @get /vacancies
-function(skill=NULL, company_id=NULL, canton=NULL, limit=100) {
-	luxJob::get_vacancies(skill, company_id, canton, as.integer(limit))
+function(res, req, skill=NULL, company_id=NULL, canton=NULL, limit=100) {
+	auth_helper(res, req, function(skill, company_id, canton, limit) {
+		luxJob::get_vacancies(skill, company_id, canton, as.integer(limit))
+	}, skill = skill, company_id = company_id, canton = canton, limit = limit)
 }
 
 #* Get vacancy by ID.
 #* @param vacancy_id:integer The ID of a particular vacancy.
 #* @get /vacancy
-function(vacancy_id) {
-	luxJob::get_vacancy_by_id(as.integer(vacancy_id))
+function(res, req, vacancy_id) {
+	auth_helper(res, req, function(vacancy_id) {
+		luxJob::get_vacancy_by_id(as.integer(vacancy_id))
+	}, vacancy_id = vacancy_id)
 }
 
 #* Get learning tracks.
 #* @param skill:string The ID of a particular skill.
 #* @get /learningtracks
-function(skill = NULL) {
-	luxJob::get_learning_tracks(skill)
+function(res, req, skill = NULL) {
+	auth_helper(res, req, function(skill) {
+		luxJob::get_learning_tracks(skill)
+	}, skill = skill)
 }
 
 #* Get a particular learning track.
 #* @param track_id:integer The ID of a particular learning track.
 #* @get /learningtrack
-function(track_id = NULL) {
-	luxJob::get_learning_track_by_id(as.integer(track_id))
+function(res, req, track_id = NULL) {
+	auth_helper(res, req, function(track_id) {
+		luxJob::get_learning_track_by_id(as.integer(track_id))
+	}, track_id = track_id)
 }
 
 #* Get a filtered list of books.
 #* @param skill:string The particular skill.
 #* @get /books
-function(skill = NULL) {
-	luxJob::get_books(skill)
+function(res, req, skill = NULL) {
+	auth_helper(res, req, function(skill) {
+		luxJob::get_books(skill)
+	}, skill = skill)
 }
 
 #* Get a particular book.
 #* @param book_id:integer The ID of a particular book.
 #* @get /book
-function(book_id = NULL) {
-	luxJob::get_book_by_id(as.integer(book_id))
+function(res, req, book_id = NULL) {
+	auth_helper(res, req, function(book_id) {
+		luxJob::get_book_by_id(as.integer(book_id))
+	}, book_id = book_id)
 }
 
 #* Post a search into the log system.
 #* @param user_id:integer The ID of a particular user.
 #* @param query:string The search string to send to the log.
 #* @post /log
-function(user_id=NULL, query=NULL) {
-	return(luxJob::log_search(as.integer(user_id), query))
+function(res, req, user_id=NULL, query=NULL) {
+	auth_helper(res, req, function(user_id, query) {
+		return(luxJob::log_search(as.integer(user_id), query))
+	}, user_id = user_id, query = query)
 }
 
-#* Add two numbers. # This is just a simple endpoint for testing POST requests.
+#* Add two numbers. # This is just a stupid simple endpoint for testing POST requests.
 #* @param a:integer A number.
 #* @param b:integer Another number.
 #* @post /add
-function(a, b) {
-	return(as.integer(a)+as.integer(b))
+function(res, req, a, b) {
+	auth_helper(res, req, function(a, b) {
+		return(as.integer(a)+as.integer(b))
+	}, a = a, b = b)
 }
